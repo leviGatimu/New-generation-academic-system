c92dfa2e11086a108e4ebf1c388f9652abeb2715
leviGatimu <getmorelev@gmail.com>
2026-01-17 16:21:48 +0200

    Final redirection hosting fixes

--- api/student/dashboard.php
+++ api/student/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require __DIR__ . '/../config/db.php';
+require __DIR__ . '/../../config/db.php';
 
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'student') {
     header("Location: ../index.php"); exit;
